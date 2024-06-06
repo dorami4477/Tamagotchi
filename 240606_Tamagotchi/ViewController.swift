0bc9bc5952b5c79d692ec7922b6a2e73594915b8
dahyun <dahyun4477@gmail.com>
2024-06-06 11:37:07 +0900

    Initial Commit

---
+++ 240606_Tamagotchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  240606_Tamagotchi
+//
+//  Created by 박다현 on 6/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
